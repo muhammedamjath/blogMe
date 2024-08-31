8744f922184f187a842430f120159b6db9c8c1e6
Amjath <muhammedamjath0@gmail.com>
2024-08-31 23:51:46 +0530

    posted blogs component created

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -3,6 +3,7 @@ import { BlogPostComponent } from "./pages/blog-post/blog-post.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 import { SingleViewComponent } from "./pages/single-view/single-view.component";
+import { CreatedBlogsComponent } from "./pages/created-blogs/created-blogs.component";
 
 export const featureRoutes:Routes=[
     {
@@ -11,7 +12,8 @@ export const featureRoutes:Routes=[
         children: [
           { path: '', component: LandingPageComponent },  
           { path: 'blogPost', component: BlogPostComponent },
-          {path:'blogViwe',component:SingleViewComponent}
+          {path:'blogViwe',component:SingleViewComponent},
+          {path:'createdblogs',component:CreatedBlogsComponent}
         ]
       }
 ]
